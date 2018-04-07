890c5637b12606d2af90d861228a6a5c2eb204ff
xhy152 <a1528279518@qq.com>
2018-04-07 15:34:35 +0800

    补充提交 “

--- bin/myCpp.c
+++ bin/myCpp.c
@@ -1,25 +1,3 @@
-int a = 0;
-float tet=0.44;
-float c;
-char ch='';
-char dh='\0';
-string stest="j\t";
-string jj="";
-c=a+tex;
 
-/*jiefiow
-   *****34*/
 while(ch>dh || tex<=c !c>1){
-	eh='k';
-	if(tex>=c){
-		a=tex-c;
-	}else if(tex < c){
-		a=tex*c;
-	}else if(tex==c){
-		a=tex/c;
-	}else if(tex!=c){
-		a=tex[8];
-	}
-	c=3;
-};
-main(c,a);
+	
--- src/com/compiler/MainClass.java
+++ src/com/compiler/MainClass.java
@@ -8,8 +8,29 @@ public class MainClass {
   public static void main(String[] args) throws IOException {
     // TODO 自动生成的方法存根
     // System.out.println(MainClass.class.getResource("/"));
-    sourceFileNameString = MainClass.class.getResource("/").getFile() + "myCpp.c";
-    LexicalAnalyzer.getToken(sourceFileNameString);
+    // sourceFileNameString = MainClass.class.getResource("/").getFile() + "myCpp.c";
+    SyntaxAnalyzer test = new SyntaxAnalyzer();
+    test.startAnalyse(
+        MainClass.class.getResource("/").getFile() + "test_token_ArithmeticExpression.txt");
+    // ArrayList<Item> a1 = new ArrayList<>();
+    // ArrayList<Item> a2 = new ArrayList<>();
+    // Item t1 = new Item(0, 1, "dji");
+    // Item t2 = new Item(0, 1, "dji");
+    // a1.add(t1);
+    // a2.add(t2);
+    // System.out.println(t1.equals(t2));
+    // System.out.println(t1.hashCode());
+    // System.out.println(t2.hashCode());
+    // System.out.println(a1.equals(a1));
+    // System.out.println(a1.hashCode());
+    // System.out.println(a1.hashCode());
+    // String[] s1 = { "s1", "s2" };
+    // String[] s2 = { "s1", "s2" };
+    // System.out.println(s1.equals(s2));
+    // System.out.println(s1.hashCode());
+    // System.out.println(s2.hashCode());
+    // System.out.println(Arrays.hashCode(s1));
+    // System.out.println(Arrays.hashCode(s2));
   }
 
 }
